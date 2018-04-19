042e9150962ad01af96490af25b4a53a42326c3f
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2018-04-19 02:52:23 -0700

    use count and array_push functions to manipulate arrays

--- learnphp.php
+++ learnphp.php
@@ -8,6 +8,14 @@
         <?php
             $x = "Hawa is a silly girl!!!";
             echo $x;
+        
+        $bestFriends = array();
+    array_push($bestFriends, "Rares");
+    array_push($bestFriends, "Cedric");
+    array_push($bestFriends, "Svetlin");
+    array_push($bestFriends, "Mom");
+    array_push($bestFriends, "Isatou");
+    print count($bestFriends);
         ?>
        
 
