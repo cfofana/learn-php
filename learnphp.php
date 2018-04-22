a1d26e8d83847fd5d460c25d1dd220e619944759
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2018-04-21 19:59:40 -0700

    add properties to person class

--- learnphp.php
+++ learnphp.php
@@ -39,11 +39,15 @@
 	
     <?php 
             class Person {
-                
+                public $isAlive = true;
+                public $firstname;
+                public $lastname;
+                public $age;
             }
             
             $teacher = new Person();
             $student = new Person();
+            echo $teacher->isAlive;
         ?>
 
     </body>
